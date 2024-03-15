f3eaa5e8569d59bb7d85287fcf0e4993f581a3e3
anggernuramin <anggern514@gmail.com>
2024-03-16 05:10:07 +0700

    feature: route /contacts

--- index.js
+++ index.js
@@ -7,6 +7,53 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/contact", (req, res) => {
+  const defaultContacts = [
+    {
+      name: "Angger Nur Amin",
+      email: "anggern514@gmail.com",
+      nohp: "088989410007",
+    },
+    {
+      name: "John doe",
+      email: "johndoe.com",
+      nohp: "08827389279832",
+    },
+    {
+      name: "Angger Nur Amin",
+      email: "anggern514@gmail.com",
+      nohp: "088989410007",
+    },
+    {
+      name: "John doe",
+      email: "johndoe.com",
+      nohp: "08827389279832",
+    },
+    {
+      name: "Angger Nur Amin",
+      email: "anggern514@gmail.com",
+      nohp: "088989410007",
+    },
+    {
+      name: "John doe",
+      email: "johndoe.com",
+      nohp: "08827389279832",
+    },
+    {
+      name: "Angger Nur Amin",
+      email: "anggern514@gmail.com",
+      nohp: "088989410007",
+    },
+    {
+      name: "John doe",
+      email: "johndoe.com",
+      nohp: "08827389279832",
+    },
+  ];
+
+  res.send(defaultContacts);
+});
+
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}/`)
 );
